7f07cee39b1d2bf5a2515f7f825bd54e2142a191
Davenchy <firon1222@gmail.com>
2023-04-25 13:42:14 +0200

    Update _strtok function to handle repeated delimiters

--- includes/utils.h
+++ includes/utils.h
@@ -1,8 +1,11 @@
 #ifndef __UTILS_S__
 #define __UTILS_S__
 
+#include <stddef.h>
 extern char **environ;
 
-char *_strtok(char *str, const char *delim);
+char *_strchr(char *str, char c);
+size_t _strspn(char *str, char *delim);
+char *_strtok(char *str, char *delim);
 
 #endif
--- utils.c
+++ utils.c
@@ -1,4 +1,40 @@
-#include <string.h>
+#include "includes/utils.h"
+
+/**
+ * _strchr - search for character in string
+ * @str: string to search in
+ * @c: character to search for
+ *
+ * Return: pointer to first accurance of @c in @str otherwise NULL
+ */
+char *_strchr(char *str, char c)
+{
+	if (!str || !c)
+		return (NULL);
+	for (; *str; str++)
+		if (*str == c)
+			return (str);
+	return (NULL);
+}
+
+/**
+ * _strspn - calc the len of the continues accurance of @delim chars in @str
+ * @str: string to search in
+ * @delim: characters to use in search
+ *
+ * Return: the calculated length
+ */
+size_t _strspn(char *str, char *delim)
+{
+	size_t size = 0;
+
+	if (!str || !delim)
+		return (0);
+	for (; *str; str++, size++)
+		if (!_strchr(delim, *str))
+			break;
+	return (size);
+}
 
 /**
  * _strtok - strtok implementation
@@ -7,26 +43,31 @@
  *
  * Return: pointer to the start of the token in @str
  */
-char *_strtok(char *str, const char *delim)
+char *_strtok(char *str, char *delim)
 {
-	char *start, *end;
-	static char *cache;
+	static char *ptr;
+	char *token;
 
+	/* set string to tokenize if any */
 	if (str)
-		cache = str;
-	if (!cache)
+		ptr = str;
+	/* if no any data to tokenize then end */
+	if (!ptr || !*ptr)
 		return (NULL);
 
-	start = cache;
-	end = strpbrk(cache, delim);
-	if (end)
-	{
-		*end = 0;
-		cache = end + 1;
-	}
-	else
-	{
-		cache = NULL;
-	}
-	return (start);
+	/* skip beginning*/
+	ptr += _strspn(ptr, delim);
+	/* if no more chars then end */
+	if (ptr && !*ptr)
+		return (NULL);
+	/* set the beginning of the token */
+	token = ptr;
+	/* go to the next delim */
+	for (; *ptr; ptr++)
+		if (_strchr(delim, *ptr))
+			break;
+	/* if not the end of string then end the token */
+	if (*ptr)
+		*ptr++ = 0;
+	return (token);
 }
